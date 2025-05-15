4ff4bfcb9203b5e41844644f7c8ba6b5a9f0f1f2
Sathira Williams <45524280+pwSathira@users.noreply.github.com>
2025-05-15 15:38:00 -0400

    Time: 39 ms (49.55%), Space: 17.9 MB (8.22%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -2,25 +2,15 @@ class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         len1 = len(word1)
         len2 = len(word2)
-        merged_chars = []  # Use a list to build the string efficiently
-        i, j = 0, 0
+        merged = []  # Use a list to build the string efficiently
+        counter = 0
 
-        # Loop while there are characters in both strings
-        while i < len1 and j < len2:
-            merged_chars.append(word1[i])
-            merged_chars.append(word2[j])
-            i += 1
-            j += 1
+        while(len1 > counter or len2 > counter):
+            if(len1 > counter):
+                merged.append(word1[counter])
+            if(len2 > counter):
+                merged.append(word2[counter])
+            counter += 1
 
-        # If word1 has remaining characters, append them
-        while i < len1:
-            merged_chars.append(word1[i])
-            i += 1
-
-        # If word2 has remaining characters, append them
-        while j < len2:
-            merged_chars.append(word2[j])
-            j += 1
-
-        return "".join(merged_chars)
+        return "".join(merged)
 
