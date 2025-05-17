c3c5e37891d2f48759e2bac39dc0ed4c99032da8
Sathira Williams <45524280+pwSathira@users.noreply.github.com>
2025-05-17 18:21:24 -0400

    Time: 0 ms (100%), Space: 17.7 MB (54.51%) - LeetHub

---
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,15 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        target = candies[0]
+        result = []
+        for i in candies: 
+            if target <= i:
+                target = i
+        
+        for j in candies:
+            if j+extraCandies >= target:
+                result.append(True)
+            else: 
+                result.append(False)
+
+        return result
