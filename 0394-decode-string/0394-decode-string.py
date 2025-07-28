0aa5d2f5fc0273a10acc650d009c7dfb3bedcfbc
Sathira Williams <45524280+pwSathira@users.noreply.github.com>
2025-07-28 09:54:53 -0700

    Time: 3 ms (3.68%), Space: 17.7 MB (84.1%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -6,19 +6,15 @@ class Solution:
 
         for char in s:
             if char.isdigit():
-                # Build the number for repeat count
                 num = num * 10 + int(char)
             elif char == "[":
-                # Push the current context to stack
                 stack.append((current_string, num))
                 current_string = ""
                 num = 0
             elif char == "]":
-                # Pop and combine the repeated substring
                 last_string, repeat_count = stack.pop()
                 current_string = last_string + current_string * repeat_count
-            else:
-                # Append character to the current string
+            else: 
                 current_string += char
-
+        
         return current_string
