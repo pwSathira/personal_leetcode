a7a66e18f51526a080125c164d02895a54a664e6
Sathira Williams <45524280+pwSathira@users.noreply.github.com>
2026-01-16 10:06:42 -0800

    Time: 1160 ms (11.4%), Space: 19.2 MB (15.08%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,10 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        
+        i = 2
+        while i * i <= x:
+            i += 1
+
+        return i - 1
